fd8291364a15099bf26efff729f60d75b830e511
Nic West <nicwest@mailbox.org>
2024-04-23 00:28:28 +0100

    nice fretboard thing now

--- out/scratch.scad
+++ out/scratch.scad
@@ -1,33 +1,109 @@
-union () {
-  cube ([60, 6, 6], center=true);
-  translate ([-35/2, 0, 0]) {
-    rotate (a=90.0, v=[1, 0, 0]) {
-      cylinder (h=12, r=0.254, center=true);
+intersection () {
+  translate ([0, 0, -3]) {
+    linear_extrude (height=6, center=true){
+      polygon (points=[[-30, 648.0], [30, 648.0], [35, 171.83885182611846], [-35, 171.83885182611846]]);
     }
   }
-  translate ([-21/2, 0, 0]) {
-    rotate (a=90.0, v=[1, 0, 0]) {
-      cylinder (h=12, r=0.3302, center=true);
+  difference () {
+    union () {
+      hull () {
+        translate ([0, 648.0, 0]) {
+          translate ([0, 0, -254]) {
+            rotate (a=90.0, v=[1, 0, 0]) {
+              cylinder ($fn=1000, h=1, r=254, center=true);
+            }
+          }
+        }
+        translate ([0, 181.83885182611846, 0]) {
+          translate ([0, 0, -406]) {
+            rotate (a=90.0, v=[1, 0, 0]) {
+              cylinder ($fn=1000, h=1, r=406, center=true);
+            }
+          }
+        }
+      }
+      hull () {
+        translate ([0, 181.83885182611846, 0]) {
+          translate ([0, 0, -406]) {
+            rotate (a=90.0, v=[1, 0, 0]) {
+              cylinder ($fn=1000, h=1, r=406, center=true);
+            }
+          }
+        }
+        translate ([0, 171.83885182611846, 0]) {
+          translate ([0, 0, -406]) {
+            rotate (a=90.0, v=[1, 0, 0]) {
+              cylinder ($fn=1000, h=1, r=406, center=true);
+            }
+          }
+        }
+      }
     }
-  }
-  translate ([-7/2, 0, 0]) {
-    rotate (a=90.0, v=[1, 0, 0]) {
-      cylinder (h=12, r=0.4318, center=true);
+    translate ([0, 611.6305546177374, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
     }
-  }
-  translate ([7/2, 0, 0]) {
-    rotate (a=90.0, v=[1, 0, 0]) {
-      cylinder (h=12, r=0.7619999999999999, center=true);
+    translate ([0, 577.3023693549399, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
     }
-  }
-  translate ([21/2, 0, 0]) {
-    rotate (a=90.0, v=[1, 0, 0]) {
-      cylinder (h=12, r=1.0668, center=true);
+    translate ([0, 544.900877084407, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
     }
-  }
-  translate ([35/2, 0, 0]) {
-    rotate (a=90.0, v=[1, 0, 0]) {
-      cylinder (h=12, r=1.3208, center=true);
+    translate ([0, 514.3179408376966, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 485.4514929080448, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 458.20519420888274, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 432.48811275109114, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 408.21442016593886, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 385.3031052608816, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 363.67770365223686, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 343.26604257241166, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 324.0, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 305.81527730886876, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 288.65118467746987, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 272.4504385422035, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 257.15897041884836, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 242.72574645402236, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 229.10259710444143, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 216.24405637554563, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 204.1072100829694, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 192.6515526304408, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
+    }
+    translate ([0, 181.83885182611846, -1/3]) {
+      cube ([1000, 3/2, 6], center=true);
     }
   }
 }
--- src/guitar_cad/core.clj
+++ src/guitar_cad/core.clj
@@ -3,133 +3,15 @@
             [scad-clj.scad :refer [write-scad]]
             [scad-clj.model :refer [polygon extrude-linear cylinder difference union
                                     translate mirror resize minkowski sphere
-                                    with-fn cube square hull rotate]])
+                                    with-fn cube square hull rotate intersection]])
   (:gen-class))
 
-(def machine-head-post-radius 3)
-(def machine-head-hole-radius 5)
-(def string-gap 7)
+(defn degrees [n] (* n (/ Math/PI 180)))
 
-(defn headstock-base
-  [{:keys [pretty?]}]
-  (let [height (if pretty? 12 16)
-        topx (if pretty? 19 21)
-        bottomx (if pretty? 46 50)
-        neckx (if pretty? 28 30)
-        raw-shape 
-        (difference
-          (extrude-linear {:height height} 
-                          (polygon [[(- topx) 0]
-                                    [topx 0]
-                                    [bottomx 150]
-                                    [(- bottomx) 150]]))
-          (translate [bottomx 150]
-                     (resize [40 60 20]
-                             (cylinder 20 20)))
-          (translate [(- bottomx) 150]
-                     (resize [40 60 20]
-                             (cylinder 20 20))))]
-    (if pretty? 
-      (minkowski raw-shape (with-fn 20 (sphere 2)))
-      raw-shape)))
-
-(def machine-head-holes
-  (union 
-      (translate [(+ string-gap machine-head-post-radius) 30]
-                 (cylinder machine-head-hole-radius 20))
-      (translate [(+ (* string-gap 2) machine-head-post-radius) 70]
-                 (cylinder machine-head-hole-radius 20))
-      (translate [(+ (* string-gap 3) machine-head-post-radius) 110]
-                 (cylinder machine-head-hole-radius 20))))
-
-(defn headstock
-  [{:keys [pretty?]}]
-  (translate [0 0 -8]
-  (difference
-    (headstock-base {:pretty? pretty?})
-    machine-head-holes
-    (mirror [1 0 0] machine-head-holes)
-    (translate [0 170 0]
-      (cube 100 20 40)))))
-
-(defn neck-profile-base
-  [{:keys [pretty?]}]
-  (rotate (- (/ Math/PI 2)) [1 0 0]
-          (difference
-            (if pretty?
-              (with-fn 100
-                       (cylinder 20 5))
-              (cylinder 20 5))
-            (translate [0 -20 0]
-                       (cube 40 40 20)))))
-
-(defn neck-profile
-  [{:keys [pretty?]} & cross-sections]
-  (hull
-    (for [[w h y] cross-sections]
-      (->> (neck-profile-base {:pretty? pretty?})
-           (resize [w 1 h])
-           (translate [0 y 0])))))
-
-(defn deg->rads [n] (* n (/ Math/PI 180)))
-
-(defn headstock-neck-joint
-  [{:keys [joint-width headstock-width headstock-height headstock-tilt-degs
-           neck-width neck-height pretty? cut-radius lump-height]}]
-  (difference
-    (hull
-      (->> (cube headstock-width 1 headstock-height)
-           (translate [0 0 (- (/ headstock-height 2))])
-           (rotate (deg->rads headstock-tilt-degs) [1 0 0]))
-      (->> (difference 
-             (cylinder (/ lump-height 2) 1)
-             (translate [0 (- (/ lump-height 2)) 0]
-                        (cube headstock-width headstock-width 2)))
-           (rotate (deg->rads -90) [1 0 0])
-           (translate [0 (* joint-width 1/5) 0]))
-      (->> (neck-profile-base {:pretty? pretty?})
-           (resize [neck-width 1 neck-height])
-           (translate [0 joint-width 0])))
-    (->> (cylinder cut-radius headstock-width)
-         (rotate (deg->rads 90) [0 1 0])
-         (translate [0 0 (- 0 cut-radius headstock-height)]))))
-
-(defn neck
-  [opts]
-  (union
-    (->> (headstock opts)
-         (translate [0 -150 0])
-         (rotate (deg->rads 5) [1 0 0])
-         (translate [0 150 0]))
-    (->> (headstock-neck-joint 
-           {:joint-width 30 :headstock-width 60
-            :headstock-height 16 :headstock-tilt-degs 5
-            :lump-height 50
-            :neck-width 60 :neck-height 20 :cut-radius 15 })
-         (translate [0 150 0]))
-    (->> (neck-profile opts [60 20 0] [70 35 400])
-         (translate [0 180 0]))))
 
 (defn gauge->mm
   [n]
   (* n 25.4))
-
-
-(defn nut
-  [{:keys [neck-width nut-width nut-height pretty?
-           strings string-spacing]}]
-  (let [offset (-> strings
-                   (count)
-                   (- 1)
-                   (/ 2)
-                   (* string-spacing)
-                   (-)) ]
-             (union
-               (cube neck-width nut-width nut-height)
-               (for [[i r] (map-indexed #(vector %1 (gauge->mm %2)) strings)]
-                 (->> (cylinder r (* 2 nut-width))
-                      (rotate (deg->rads 90) [1 0 0])
-                      (translate [(+ offset (* i string-spacing)) 0 0]))))))
 
 (defn render!
   [file-name part]
@@ -138,23 +20,85 @@
     (spit file-path
           (write-scad part))))
 
-(def default-config
-  {:pretty? false
-   :strings [0.010 0.013 0.017 0.030 0.042 0.052]
-   :scale-length 22
-   :fretboard-radius [[0 254] [22 406]]
-   :neck-radius [[0 60] [0 70]]
-   :neck-wdith [[0 60] [0 70]]})
 
-(render! "headstock" (headstock {:pretty? false}))
+(defn- calc-fret-spacing
+  [fret scale-length]
+  (/ scale-length (Math/pow 2 (/ fret 12))))
 
-(render! "neck-profile" (neck-profile {:pretty? false} [40 10 0] [50 15 200]))
+(defn- fretboard-profile
+  [radius]
+  (with-fn 1000
+  (->> (cylinder radius 1)
+       (rotate (degrees 90) [1 0 0])
+       (translate [0 0 (- radius)]))))
 
-(render! "neck" (neck {:pretty? false}))
+(defn fretboard-cutter
+  [{:keys [fretboard-width fret-spacing fretboard-height fretboard-end-padding]}]
+  (let [fret-zero-y (first fret-spacing)
+        fret-last-y (- (last fret-spacing) fretboard-end-padding)
+        fret-zero-x (/ (last (first fretboard-width)) 2)
+        fret-last-x (/ (last (last fretboard-width)) 2)]
+    (->> (extrude-linear {:height fretboard-height}
+                    (polygon [[(- fret-zero-x) fret-zero-y]
+                              [fret-zero-x fret-zero-y]
+                              [fret-last-x fret-last-y]
+                              [(- fret-last-x) fret-last-y] ]))
+         (translate [0 0 (- (/ fretboard-height 2))]))))
 
-(render! "scratch" (nut {:pretty? false :neck-width 60 :nut-width 6 :nut-height 6
-                         :strings [0.010 0.013 0.017 0.030 0.042 0.052]
-                         :string-spacing 7}))
+(defn fretboard
+  [{:keys [fretboard-radius fretboard-height fret-tang-height fret-tang-width
+           fret-spacing fretboard-end-padding] :as opts}]
+  (intersection
+    (fretboard-cutter opts)
+    (difference
+      (union
+        (for [[[f1 r1] [f2 r2]] (drop-last (partition 2 1 fretboard-radius fretboard-radius))]
+          (hull 
+            (->> (fretboard-profile r1)
+                 (translate [0 (nth fret-spacing f1) 0]))
+            (->> (fretboard-profile r2)
+                 (translate [0 (nth fret-spacing f2) 0]))))
+
+        ; add a little padding to the end of the fretboard
+        (let [r (last (last fretboard-radius))
+              y (last fret-spacing)]
+          (hull
+            (->> (fretboard-profile r)
+                 (translate [0 y 0]))
+            (->> (fretboard-profile r)
+                 (translate [0 (- y fretboard-end-padding) 0])))))
+
+      (for [fret (drop 1 fret-spacing)]
+        (->> (cube 1000 fret-tang-width fret-tang-height)
+             (translate [0 fret (- (/ 2 fret-tang-height))]))))))
+
+
+(defn config
+  ([] (config {}))
+  ([opts]
+  (let [opts (merge {:pretty? false
+                     :strings [0.010 0.013 0.017 0.030 0.042 0.052]
+                     :frets 22
+                     :fret-tang-height 6
+                     :fret-tang-width 3/2
+                     :scale-length 648
+                     :fretboard-radius [[0 254] [22 406]]
+                     :fretboard-width [[0 60] [22 70]]
+                     :fretboard-height 6
+                     :fretboard-end-padding 10
+                     :neck-radius [[0 60] [22 70]]
+                     }
+                    opts)
+        opts (assoc opts
+                    :fret-spacing 
+                    (into [] (map-indexed calc-fret-spacing
+                                               (repeat
+                                                 (+ 1 (:frets opts))
+                                                 (:scale-length opts)))))]
+    opts)))
+
+
+(render! "scratch" (fretboard (config)))
 
 (defn -main
   "I don't do a whole lot ... yet."
